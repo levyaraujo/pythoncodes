660a0d2875a32129a939f8fdbde8b4e2168205e1
Levy Araujo <levya95@gmail.com>
2020-06-19 17:04:48 -0300

    Criando gerador usando funçao

--- funcao_geradora.py
+++ funcao_geradora.py
@@ -2,7 +2,7 @@ from time import sleep
 
 def gerador():  # Esta função chama-se função geradora
     for v in range(1000):
-        yield v  # yield é a instrução que carrega na memória e retorna um valor por vez
+        yield v  # yield é a instrução que gera o gerador e retorna os valores sob demanda
         sleep(0.1)
 
 g = gerador()
