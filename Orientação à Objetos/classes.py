3716e5cf95a1d166d14ebce2efc78a05bde03649
Levy Araujo <levya95@gmail.com>
2020-06-29 18:41:00 -0300

    Introducao a POO em Python

--- Orientação à Objetos/classes.py
+++ Orientação à Objetos/classes.py
@@ -5,64 +5,75 @@ class Pessoa:
     ano_atual = int(datetime.strftime(datetime.now(), '%Y'))
 
     # Funções dentro de classes são chamadas de métodos
-    def __init__(self, nome, idade, comendo=False, falando=False):
-        self.nome = nome
+    def __init__(self, name, idade, comendo=False, falando=False, premium=False):
+        self.name = name
         self.idade = idade
         self.comendo = comendo
         self.falando = falando
+        self.premium = premium
 
     def falar(self, assunto):
         if self.comendo:
-            print(f'{self.nome} não pode falar porque está comendo.')
+            print(f'{self.name} não pode falar porque está comendo.')
             return
 
         if self.falando:
-            print(f'{self.nome} já está falando.')
+            print(f'{self.name} já está falando.')
             return
 
-        print(f'{self.nome} está falando sobre {assunto}.')
+        print(f'{self.name} está falando sobre {assunto}.')
         self.falando = True
 
     def parar_falar(self):
         if not self.falando:
-            print(f'{self.nome} não está falando')
+            print(f'{self.name} não está falando')
             return
 
-        print(f'{self.nome} parou de falar.')
+        print(f'{self.name} parou de falar.')
         self.falando = False
 
     def comer(self, alimento):
         if self.comendo:
-            print(f'{self.nome} já está comendo.')
+            print(f'{self.name} já está comendo.')
             return
 
         if self.falando:
-            print(f'{self.nome} não pode comer pois está falando.')
+            print(f'{self.name} não pode comer pois está falando.')
             return
 
-        print(f'{self.nome} está comendo {alimento}.')
+        print(f'{self.name} está comendo {alimento}.')
         self.comendo = True
 
     def parar_comer(self):
         if not self.comendo:
-            print(f'{self.nome} não está comendo.')
+            print(f'{self.name} não está comendo.')
             return
 
-        print(f'{self.nome} parou de comer.')
+        print(f'{self.name} parou de comer.')
         self.comendo = False
 
     def get_ano_nascimento(self):
-        print(f'{self.nome} nasceu em {self.ano_atual - self.idade}')
+        print(f'{self.name} nasceu em {self.ano_atual - self.idade}')
 
+    def is_premium(self):
+        if self.name == 'Levy':
+            self.premium = True
+            print(f'{self.name} é um usuário premium')
+        else:
+            print(f'{self.name} não é um usuário premium')
+
+        self.premium = False
 
 # Cada pessoa é uma instância da Classe Pessoa e tem suas próprias
 # características (atributos)
+
 
 pessoa1 = Pessoa('Levy', 19)
 pessoa1.comer('abacate')
 pessoa1.parar_comer()
 pessoa1.falar('programação')
 pessoa1.get_ano_nascimento()
+pessoa1.is_premium()
 
 print('-'*50)
 
@@ -71,3 +82,4 @@ pessoa2.comer('melancia')
 pessoa2.parar_comer()
 pessoa2.falar('séries')
 pessoa2.get_ano_nascimento()
+pessoa2.is_premium()
